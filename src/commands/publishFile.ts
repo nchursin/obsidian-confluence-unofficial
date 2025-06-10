df2936af3ca7c94f1776bfd41fd595563a7fe3fc
Nikita Chursin <nichursin@ozon.ru>
2025-06-10 19:49:13 +0300

    images work

--- src/commands/publishFile.ts
+++ src/commands/publishFile.ts
@@ -15,7 +15,17 @@ const getHtml = async (app: App, md: string, view: MarkdownView) => {
 	const renderDiv = view.contentEl.createEl("div", {});
 	await MarkdownRenderer.render(app, md, renderDiv, "nop", component);
 
-	return renderDiv.innerHTML;
+	return renderDiv.innerHTML.replace(
+		/<img([^>]*)>/g,
+		function (match, attributes) {
+			// Check if it's already properly closed
+			if (match.trim().endsWith("/>")) {
+				return match;
+			}
+			// Otherwise close it properly
+			return `<img${attributes} />`;
+		},
+	);
 };
 
 export const publishFile = (plugin: ConfluencePlugin): Command => ({
@@ -29,11 +39,38 @@ export const publishFile = (plugin: ConfluencePlugin): Command => ({
 			},
 		});
 
-		await confluenceClient.upsertPage({
-			spaceKey: plugin.settings.spaceKey,
+		if (!view.file) {
+			return;
+		}
+		let pageId, spaceKey;
+		let version = 1;
+		await plugin.app.fileManager.processFrontMatter(
+			view.file,
+			(fronmatter) => {
+				pageId = fronmatter.confluence_page_id;
+				spaceKey = fronmatter.confluence_space;
+				version = fronmatter.confluence_page_version;
+			},
+		);
+
+		const stringResponse = await confluenceClient.upsertPage({
+			pageId: pageId,
+			spaceKey: spaceKey || plugin.settings.spaceKey,
 			title: view.getDisplayText(),
 			parentId: plugin.settings.parentId,
 			htmlContent: await getHtml(plugin.app, view.getViewData(), view),
+			version: version ? version + 1 : 1,
 		});
+
+		const response = JSON.parse(stringResponse);
+
+		await plugin.app.fileManager.processFrontMatter(
+			view.file,
+			(fronmatter) => {
+				fronmatter.confluence_page_id = response.id;
+				fronmatter.confluence_space = response.space.key;
+				fronmatter.confluence_page_version = response.version.number;
+			},
+		);
 	},
 });
