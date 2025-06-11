828a98530d1758d0de48d970a83f7a5add14ab3e
Nikita Chursin <nchursin.dev@gmail.com>
2025-06-11 09:22:54 +0300

    refactor: moved md to html conversion to a separate file

---
+++ src/utils/htmlProcessor.ts
@@ -0,0 +1,28 @@
+import { App, Component, MarkdownRenderer, MarkdownView } from "obsidian";
+
+export const convertMdToHtml = async (
+	app: App,
+	md: string,
+	view: MarkdownView,
+) => {
+	const component = new Component();
+	component.load();
+	const renderDiv = view.contentEl.createEl("div", {});
+	await MarkdownRenderer.render(app, md, renderDiv, "nop", component);
+
+	return downgradeFromHtml5(renderDiv.innerHTML);
+};
+
+export const downgradeFromHtml5 = (html: string) =>
+	html
+		.replace(/<img([^>]*)>/g, function (match, attributes) {
+			// Check if it's already properly closed
+			if (match.trim().endsWith("/>")) {
+				return match;
+			}
+			// Otherwise close it properly
+			return `<img${attributes} />`;
+		})
+		.replace(/<br>/g, function (match, attributes) {
+			return `<br/>`;
+		});
